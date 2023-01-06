fe4774ed92bbae6f4deef4ad20d0144fb4f4ef80
Adryan Almeida <me@adryan.me>
2023-01-06 14:55:26 -0300

    :sparkles: adds healthcheck to app

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "ok" to healthcheck', () => {
+      expect(appController.getHealthcheck()).toBe('ok!');
     });
   });
 });
